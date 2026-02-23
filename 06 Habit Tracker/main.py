82f00bf7f2407a9ed994cf7d1c4a1c55d85e68a0
Sairaj Jadhav <jadhavsairaj25@gmail.com>
2026-02-23 15:49:52 +0530

    refactor: separate user creation and graph setup logic

--- 06 Habit Tracker/main.py
+++ 06 Habit Tracker/main.py
@@ -1,54 +1,40 @@
 import requests
 from datetime import datetime
+from dotenv import load_dotenv  # pyright: ignore[reportMissingImports]
+import os
 
-Token = "samsndi0453jedi"
-UserName = "sairaj0"
-Graph_ID = "graph1"
+# Load environment variables
+load_dotenv()
+
+TOKEN = os.getenv("PIXELA_TOKEN")
+USERNAME = os.getenv("PIXELA_USERNAME")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": Token,
-    "username": UserName,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
+    "token" : TOKEN,
+    "username" : USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor":"yes"
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
+
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/{UserName}/graphs"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": Graph_ID,
-    "name": "Cycling Graph",
-    "unit": "km",
+    "id": "vscode1",
+    "name": "VS Code Daily Usage",
+    "unit": "hours",
     "type": "float",
-    "color": "ajisai"
+    "color": "ajisai"  # purple (like GitHub green alternative)
 }
-
-# for Security api Key is provided in headers
 
 headers = {
-    "X-USER-TOKEN": Token
+    "X-USER-TOKEN": TOKEN
 }
 
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-#
-# print(response.text)
-
-pixel_creation_endpoint = f"{pixela_endpoint}/{UserName}/graphs/{Graph_ID}"
-
-# today = datetime.now() # 2024-09-26 16:46:39.250589
-
-today = datetime(year=2024, month=9, day=25)
-print(today.strftime("%Y%m%d")) # 20240926 --> using strftime() Method
-
-pixel_data = {
-    "date": today.strftime("%Y%m%d"),
-    "quantity": "9.54",
-}
-
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
-
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
